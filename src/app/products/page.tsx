f657df477ff7f9f83a9ec4e182636cf2448283d6
maqsooddw <155903860+maqsooddw@users.noreply.github.com>
2025-02-07 18:09:04 +0500

    twicks

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,27 +1,35 @@
-"use client";
-import React from 'react';
+"use client"; 
+import React, { useEffect, useState } from 'react';
 import Everypageupperbox from '../components/Everypageupperbox';
 import Submenuofproducts from '../components/Submenuofproducts';
-import ProductList from '../components/Productlist'; // Corrected import
-import { getImage } from '../Getdata';
+import ProductList from '../components/Productlist'; 
+import { getImage } from '../Getdata'; 
 import Image from 'next/image';
 
+export default function ProductsPage() {
+  const [products, setProducts] = useState([]);
 
-export default async function products() { // Changed to default export and async function
-  const products = await getImage();
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedProducts = await getImage();
+      setProducts(fetchedProducts);
+    };
+
+    fetchData();
+  }, []);
+
   console.log(products);
 
   return (
     <div>
       <Everypageupperbox text="Shop Grid Default" />
       <Submenuofproducts />
-      <ProductList  />
+      <ProductList />
 
       {/* start signature icon div */}
       <div className='flex items-center justify-center w-full h-fit mt-20 mb-20'>
         <div className='flex items-center justify-center'>
-               <Image src='/image 1174.png' alt='signature div' width={904} height={93} />
-
+          <Image src='/image 1174.png' alt='signature div' width={904} height={93} />
         </div>
       </div>
     </div>
